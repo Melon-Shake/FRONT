ac0df5e2eb303891deb01f6ab96be100efa86e7d
mews2000 <mews2000@gmail.com>
2023-09-01 14:16:54 +0900

    dev merge complete

---
+++ src/main/java/com/example/melon_shake_webapp/MelonShakeWebappApplication.java
@@ -0,0 +1,13 @@
+package com.example.melon_shake_webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MelonShakeWebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MelonShakeWebappApplication.class, args);
+	}
+
+}
